c76e97ffe3fde749cc496d9253d20c392b042eec
hamish.kirkman <hdk0036@arastudent.ac.nz>
2020-07-01 16:43:09 +1200

    added login and register pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login'});
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Login'});
+});
 module.exports = router;
